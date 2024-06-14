0028b63b48c1587bc555ad18effd3fb07dffea70
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2024-06-14 15:21:46 +0200

    Adding truncation to arbitrary number of bits

--- main.cpp
+++ main.cpp
@@ -6,24 +6,29 @@
 using namespace std;
 
 const size_t V=64*64*64*128*12;
-//const size_t V=64;
 
 using Compl=
   complex<double>;
 
-template <typename T>
+double truncateToNbits(const double& x,
+		    const size_t& N)
+{
+  const double t=
+    (1lu<<(53-N))*x;
+  
+  return t-(t-x);
+}
+
 struct Vect :
-  vector<complex<T>>
+  vector<complex<double>>
 {
   const string t;
   
   Vect(const size_t& v,
-       const string& t,
-       mt19937_64& gen) :
-    vector<complex<T>>(v),
+       const string& t) :
+    vector<complex<double>>(v),
     t(t)
   {
-    fill(gen);
   }
   
   void fill(mt19937_64& gen)
@@ -33,9 +38,9 @@ struct Vect :
     cout<<"Vector "<<t<<" filled"<<endl;
   }
   
-  complex<T> getProjectionWith(const Vect& oth) const
+  complex<double> getProjectionWith(const Vect& oth) const
   {
-    complex<T> s=0;
+    complex<double> s=0;
     
     for(size_t i=0;i<this->size();i++)
       s+=conj(oth[i])*(*this)[i];
@@ -43,9 +48,9 @@ struct Vect :
     return s;
   }
   
-  T norm2() const
+  double norm2() const
   {
-    T s=0;
+    double s=0;
     
     for(size_t i=0;i<this->size();i++)
       s+=norm((*this)[i]);
@@ -55,47 +60,72 @@ struct Vect :
   
   void normalize()
   {
-    const T aa=
+    const double aa=
       norm2();
     cout<<"|"<<t<<"|^2: "<<aa<<endl;
     
-    const T a=
+    const double a=
       sqrt(aa);
     
     for(auto& c : *this)
       c/=a;
     
-    cout<<t<<" normalized"<<endl;
+    cout<<t<<" normalized, check |"<<t<<"|^2-1: "<<norm2()-1<<endl;
   }
   
   void projectWith(const Vect& oth)
   {
-    const complex<T> p=
+    const complex<double> p=
       getProjectionWith(oth);
     cout<<t<<" projection on "<<oth.t<<": "<<p<<endl;
     
     for(size_t i=0;i<this->size();i++)
       (*this)[i]-=p*oth[i];
-    cout<<t<<" projected away from "<<oth.t<<endl;
+    cout<<t<<" projected away from "<<oth.t<<", residual projection: "<<getProjectionWith(oth)<<endl;
   }
   
-  void truncateToFloat()
+  Vect truncateToFloat() const
   {
-    for(auto& c : *this)
-      c=complex<float>(c);
-    cout<<t<<" truncated"<<endl;
+    Vect out(this->size(),t+"f");
+    
+    for(size_t i=0;i<this->size();i++)
+      out[i]=complex<float>((*this)[i]);
+    
+    cout<<t<<" truncated to float"<<endl;
+    
+    return out;
   }
+  
+  Vect truncateToNbits(const size_t& N) const
+  {
+    Vect out(this->size(),t+"f");
+    
+    for(size_t i=0;i<this->size();i++)
+      out[i]=complex<double>(::truncateToNbits((*this)[i].real(),N),
+			     ::truncateToNbits((*this)[i].imag(),N));
+    
+    return out;
+  }
+  
+  Vect(const Vect&)=default;
 };
 
 
 int main()
 {
+  cout.precision(16);
+  // cout<<M_PI<<" "<<endl;
+  // cout<<(float)M_PI<<endl;
+  // cout<<truncateToNbits(M_PI,24)<<endl;
+  
   mt19937_64 gen(235235523);
   cout<<"Volume: "<<V<<endl;
   
-  Vect<double> a(V,"a",gen);
+  Vect a(V,"a");
+  a.fill(gen);
   
-  Vect<double> b(V,"b",gen);
+  Vect b(V,"b");
+  b.fill(gen);
   
   a.normalize();
   
@@ -107,12 +137,29 @@ int main()
     a.getProjectionWith(b);
   cout<<"Verify projection: "<<pp<<endl;
   
-  for(auto & v : {&a,&b})
-    v->truncateToFloat();
+  const Vect af=
+    a.truncateToFloat();
+  
+  const Vect bf=
+    b.truncateToFloat();
   
   const Compl ppf=
-    a.getProjectionWith(b);
+    af.getProjectionWith(bf);
   cout<<"Projection after cast to float: "<<ppf<<endl;
+  
+  for(size_t nBits=24;nBits>=10;nBits--)
+    {
+      const Vect at=
+	a.truncateToNbits(nBits);
+      
+      const Vect bt=
+	b.truncateToNbits(nBits);
+      
+      const Compl ppt=
+	at.getProjectionWith(bt);
+      
+      cout<<"Projection after cast to "<<nBits<<" bits: "<<ppt<<endl;
+    }
   
   return 0;
 }
